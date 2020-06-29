ef17d643a338f8c00f37b6abbea907d1e3e944a8
Shreenivas PC <shree.mandpe@gmail.com>
2020-06-29 17:23:24 +0530

    Utils now strips mention from message

--- modules/bot_utils.py
+++ modules/bot_utils.py
@@ -5,15 +5,17 @@ from . import bot_config
 
 # import bot_config
 
-def get_rasa_response(user_message, user_id):
+def get_rasa_response(user_message: str, user_id: str):
+    
+    print(user_message)
+    user_message = user_message.replace("<@!726886125440073738> ", "")
+    print(user_message)
+    
     url = bot_config.RASA_URL
-    data = {
-	"sender": str(user_id),
-	"message": user_message
-    }
+    data = {"sender": str(user_id), "message": user_message}
 
     responses = []
-    
+
     try:
         response_data = requests.post(url, json=data).json()    
         responses = [r["text"] for r in response_data]
