83cb11c078bbdf844effc864c00fc974d6c7c089
Shreenivas PC <shree.mandpe@gmail.com>
2020-07-06 13:40:43 +0530

    Added weather conversions

--- actions.py
+++ actions.py
@@ -13,6 +13,35 @@ class ActionConvertTemperature(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-        data = tracker.latest_message['entities'][0]["additional_info"]
+        try: 
+            data = tracker.latest_message['entities'][0]["additional_info"]
+        
+        except: 
+            data = dispatcher.utter_message("No.")
+            return[]
+
+        
+        unit = None
+        value = data["value"]
+
+        if "unit" in data:
+            print("Found data.")
+            unit = data["unit"]
+
+            if unit == "celsius":
+                print("Found Celsius.")
+                converted_value = (value * 9/5) + 32
+                response = f"**{value}°C** is **{converted_value}°F**."
+                dispatcher.utter_message(response)
+            
+            elif unit == "fahrenheit":
+                print("Found Fahrenheit.")
+                converted_value = (value - 32) * 5/9
+                response = f"**{value}°C** is **{converted_value}°F**."
+                dispatcher.utter_message(response)
+
+        else:
+            dispatcher.utter_message("Sorry, I know you're trying to convert temperatures, but I can't figure out the specifics. Try something like `What's 5C in F?` or `convert 34 farenheit to celsius`.")
+
         print(data)
         return []
